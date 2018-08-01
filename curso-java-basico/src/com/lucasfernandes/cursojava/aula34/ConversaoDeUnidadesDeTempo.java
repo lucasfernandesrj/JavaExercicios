7976e6f06272b24615d5dd8858a60e546c2feeb3
lucas <lucas_rfernandes@hotmail.com>
2018-08-01 13:32:00 -0300

    Java

--- curso-java-basico/src/com/lucasfernandes/cursojava/aula34/ConversaoDeUnidadesDeTempo.java
+++ curso-java-basico/src/com/lucasfernandes/cursojava/aula34/ConversaoDeUnidadesDeTempo.java
@@ -2,6 +2,26 @@ package com.lucasfernandes.cursojava.aula34;
 
 public class ConversaoDeUnidadesDeTempo {
 	public static double converterMinutoParaSegundo(double num1) {
-		return num1;
+		return num1*60;
+	}
+	
+	public static double converterHoraParaMinutos(double num1) {
+		return num1*60;
+	}
+	
+	public static double converterDiaParHora(double num1) {
+		return num1*24;
+	}
+	
+	public static double converterSemanaParaDia(double num1) {
+		return num1*7;
+	}
+	
+	public static double converterMesParaDia(double num1) {
+		return num1*30;
+	}
+	
+	public static double converterAnoParaDia(double num1) {
+		return num1*365.25;
 	}
 }
---
+++ curso-java-basico/src/com/lucasfernandes/cursojava/aula34/Exercicio06.java
@@ -0,0 +1,17 @@
+package com.lucasfernandes.cursojava.aula34;
+
+public class Exercicio06 {
+
+	public static void main(String[] args) {
+		double num1 = 1;
+		
+		System.out.println(ConversaoDeUnidadesDeTempo.converterMinutoParaSegundo(num1));
+		System.out.println(ConversaoDeUnidadesDeTempo.converterHoraParaMinutos(num1));
+		System.out.println(ConversaoDeUnidadesDeTempo.converterDiaParHora(num1));
+		
+		System.out.println(ConversaoDeUnidadesDeTempo.converterSemanaParaDia(num1));
+		System.out.println(ConversaoDeUnidadesDeTempo.converterMesParaDia(num1));
+		System.out.println(ConversaoDeUnidadesDeTempo.converterAnoParaDia(num1));
+	}
+
+}
